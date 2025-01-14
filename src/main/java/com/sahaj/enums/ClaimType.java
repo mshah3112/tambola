13841873a3bc7e795e6140e913c5d7541eb12776
Mayur Shah <mayurshah3112@gmail.com>
2025-01-14 14:39:33 +0530

    Create ClaimType.java

---
+++ src/main/java/com/sahaj/enums/ClaimType.java
@@ -0,0 +1,13 @@
+package com.sahaj.enums;
+
+/**
+ * The ClaimType enum represents different types of claims that can be made. 
+ * Each type corresponds to a specific pattern or condition in a game.
+ */
+public enum ClaimType {
+    TOP_LINE,
+    MIDDLE_LINE,
+    BOTTOM_LINE,
+    EARLY_FIVE,
+    FULL_HOUSE
+}
