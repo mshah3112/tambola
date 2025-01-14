2e91db6d83050a3c71f55e3575acd768b4d9b53e
Mayur Shah <mayurshah3112@gmail.com>
2025-01-14 14:42:16 +0530

    Create constant.java

---
+++ src/main/java/com/sahaj/util/constant.java
@@ -0,0 +1,14 @@
+package com.sahaj.util;
+
+/**
+ * The Constant interface provides constants used for configuring
+ * application-specific settings. These constants define various row positions
+ * and sizes within a grid or table structure.
+ */
+public interface Constant {
+    int TOP_ROW = 0;
+    int MIDDLE_ROW = 1;
+    int BOTTOM_ROW = 2;
+    int ROW_SIZE = 3;
+    int COLUMN_SIZE = 5;
+}
